dd608b70c139efbd8145856edd89b638378df9ca
pratheebak <muralipratheeba123@gmail.com>
2026-06-04 15:39:31 +0530

    unhide files

--- Demo/tests/table.spec.js
+++ Demo/tests/table.spec.js
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-  // test('get table data', async ({ page }) => {
-  // await page.goto('https://www.tutorialspoint.com/selenium/practice/webtables.php');
-  // const rows=await page.locator('//div[@class="bd-example table-responsive"]').allTextContents();
-  // const counts=rows.count();
-  // console.log(counts);
-  //  console.log(counts);
-  //  console.log(counts);
+  test('get table data', async ({ page }) => {
+  await page.goto('https://www.tutorialspoint.com/selenium/practice/webtables.php');
+  const rows=await page.locator('//div[@class="bd-example table-responsive"]').allTextContents();
+  const counts=rows.count();
+  console.log(counts);
+   console.log(counts);
+   console.log(counts);
   console.log()
 
   });
