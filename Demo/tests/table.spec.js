653473eb1c859ad269e118b32db53ac0a7f3bb4b
pratheebak <muralipratheeba123@gmail.com>
2026-06-04 16:10:52 +0530

    update branch

--- Demo/tests/table.spec.js
+++ Demo/tests/table.spec.js
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test';
   test('get table data', async ({ page }) => {
   await page.goto('https://www.tutorialspoint.com/selenium/practice/webtables.php');
   const rows=await page.locator('//div[@class="bd-example table-responsive"]').allTextContents();
-  const counts=rows.count();
-  console.log(counts);
-   console.log(counts);
-   console.log(counts);
-  console.log()
+  // const counts=rows.count();
+  // console.log(counts);
+  //  console.log(counts);
+  //  console.log(counts);
+  // console.log()// branch
 
   });
